e129cc94636d36d9ab6b8a1ad911a36de63ff806
Gemela Juraj <J.Gemela@consultant.crif.com>
2023-12-05 14:35:11 +0100

    05: part 2 extremely slow

--- 2023/05/seed_fertilizer.js
+++ 2023/05/seed_fertilizer.js
@@ -16,22 +16,22 @@ function parseStringToIntegers (numbersString) {
   return numberArray
 }
 
-async function resolverPart1(fileName) {
-  let linesArray = readFileByLines(fileName)
-  const seeds = parseStringToIntegers(linesArray[0].split(':')[1])
+function resolveSeedFertilizer(linesArray, seeds) {
   let resultArray = seeds
   let mappedSeeds = undefined
   linesArray = linesArray.slice(1)
 
+  // TODO: optimize not going trough all the lines everytime but create one mapper per all levels
   for (let lineIndex = 0; lineIndex < linesArray.length; lineIndex++) {
     const line = linesArray[lineIndex]
-    if (line === '\r') {
-      // When finding empty line then do remap
+    // Empty line means new mapping will begin
+    if (line === '\r' || line === '') {
       if(mappedSeeds !== undefined) {
-        // Fill missing ones
+        // Saved result after remapping
         resultArray = Object.values(mappedSeeds)
       }
-      // Skip next string line
+
+      // Fill missing ones and skip next description line
       mappedSeeds = Object.fromEntries(resultArray.map(i => [i, i]))
       lineIndex++
     }
@@ -49,8 +49,51 @@ async function resolverPart1(fileName) {
 
   resultArray = Object.values(mappedSeeds)
 
-  console.log(`For file name ${fileName} => locations are: ${resultArray} and min is: ${Math.min(...resultArray)}`);
+  return Math.min(...resultArray)
+}
+
+async function resolverPart1(fileName) {
+  let linesArray = readFileByLines(fileName)
+  const seedString = linesArray[0].split(':')[1]
+
+  const seeds = parseStringToIntegers(seedString)
+  const result = resolveSeedFertilizer(linesArray, seeds)
+
+  console.log(`For file name ${fileName} => min location is: ${result}`);
+}
+
+const SEED_MAX_LIMIT = 1000
+
+function resolverPart2(fileName) {
+  let overallResult = Number.MAX_SAFE_INTEGER
+  let linesArray = readFileByLines(fileName)
+  const seedsRange = parseStringToIntegers(linesArray[0].split(':')[1])
+
+  for (let seedsRangeIndex = 0; seedsRangeIndex < seedsRange.length;) {
+    const seeds = []
+    const seedRangeStart = seedsRange[seedsRangeIndex]
+    const seedRangeLength = seedsRange[seedsRangeIndex + 1]
+    
+    for (let seedsGeneratorIndex = seedRangeStart; seedsGeneratorIndex < seedRangeStart + seedRangeLength; seedsGeneratorIndex++) {
+      seeds.push(seedsGeneratorIndex)
+    }
+
+    for (let seedIndex = 0; seedIndex < seeds.length;) {
+      console.log(seedIndex, seeds.length)
+      const partialSeeds = seeds.slice(seedIndex, seedIndex + SEED_MAX_LIMIT)
+      const result = resolveSeedFertilizer(linesArray, partialSeeds)
+      if(result < overallResult) overallResult = result
+
+      seedIndex = seedIndex + SEED_MAX_LIMIT
+    }
+
+    seedsRangeIndex = seedsRangeIndex + 2
+  }
+
+  const result = resolveSeedFertilizer(linesArray, seeds)
+
+  console.log(`For file name ${fileName} => min location is: ${result}`);
 }
 
 resolverPart1('input_1');
-// resolverPart2('input_2');
+resolverPart2('input_2');
