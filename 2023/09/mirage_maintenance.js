c797e124ed233edbf97912deddf7a0f1f1ad4ca4
Gemela Juraj <J.Gemela@consultant.crif.com>
2023-12-21 10:08:47 +0100

    09: part 2

--- 2023/09/mirage_maintenance.js
+++ 2023/09/mirage_maintenance.js
@@ -16,42 +16,77 @@ function parseStringToIntegers (numbersString) {
   return numberArray
 }
 
+function createExtrapolateDictionary(splittedHistory) {
+  const extrapolateDictionary = { 0: splittedHistory }
+  let extrapolateIndex = 0
+
+  // Create extrapolate dictionary level by level
+  while(extrapolateDictionary[extrapolateIndex].find(item => item !== 0)) {
+    const newExtrapolateLevel = []
+
+    for(let index = 1;  index < extrapolateDictionary[extrapolateIndex].length; index++) {
+      newExtrapolateLevel.push(extrapolateDictionary[extrapolateIndex][index] - extrapolateDictionary[extrapolateIndex][index - 1])
+    }
+
+    extrapolateIndex++
+    extrapolateDictionary[extrapolateIndex] = newExtrapolateLevel
+  }
+
+  return extrapolateDictionary
+}
+
+function calculateNextItem(extrapolateDictionary) {
+  let historySum = 0
+  let extrapolateIndex = Object.keys(extrapolateDictionary).length - 1
+
+  // Count sum of last items in reverse
+  while(extrapolateIndex >= 0) {
+    const lastItemIndex = extrapolateDictionary[extrapolateIndex].length - 1
+    historySum = historySum + extrapolateDictionary[extrapolateIndex][lastItemIndex]
+    extrapolateIndex--
+  }
+
+  return historySum
+}
+
+function calculatePreviousItem(extrapolateDictionary) {
+  let historySum = 0
+  let extrapolateIndex = Object.keys(extrapolateDictionary).length - 1
+
+  // Count difference of first items in reverse
+  while(extrapolateIndex >= 0) {
+    historySum = extrapolateDictionary[extrapolateIndex][0] - historySum
+    extrapolateIndex--
+  }
+
+  return historySum
+}
+
 async function resolverPart1(fileName) {
   const histories = readFileByLines(fileName)
   let historyNextValuesSum = 0
 
   for(const history of histories) {
     const splittedHistory = parseStringToIntegers(history)
+    const extrapolateDictionary = createExtrapolateDictionary(splittedHistory)
+    historyNextValuesSum = historyNextValuesSum + calculateNextItem(extrapolateDictionary) 
+  }
 
-    const extrapolateDictionary = { 0: splittedHistory }
-    let extrapolateIndex = 0
-    let historySum = 0
+   console.log(`For file name ${fileName} total counter is ${historyNextValuesSum}`);
+}
 
-    // Create extrapolate dictionary
-    while(extrapolateDictionary[extrapolateIndex].find(item => item !== 0)) {
-      const newExtrapolateLevel = []
+async function resolverPart2(fileName) {
+  const histories = readFileByLines(fileName)
+  let historyNextValuesSum = 0
 
-      for(let index = 1;  index < extrapolateDictionary[extrapolateIndex].length; index++) {
-        newExtrapolateLevel.push(extrapolateDictionary[extrapolateIndex][index] - extrapolateDictionary[extrapolateIndex][index - 1])
-      }
-
-      extrapolateIndex++
-      extrapolateDictionary[extrapolateIndex] = newExtrapolateLevel
-    }
-
-    // Count sum of last items in reverse
-    while(extrapolateIndex >= 0) {
-      const lastItemIndex = extrapolateDictionary[extrapolateIndex].length - 1
-      historySum = historySum + extrapolateDictionary[extrapolateIndex][lastItemIndex]
-      extrapolateIndex--
-    }
-
-    historyNextValuesSum = historyNextValuesSum + historySum
-    
+  for(const history of histories) {
+    const splittedHistory = parseStringToIntegers(history)
+    const extrapolateDictionary = createExtrapolateDictionary(splittedHistory)
+    historyNextValuesSum = historyNextValuesSum + calculatePreviousItem(extrapolateDictionary) 
   }
 
    console.log(`For file name ${fileName} total counter is ${historyNextValuesSum}`);
 }
 
 resolverPart1('input');
-// resolverPart2('input2');
+resolverPart2('input');
